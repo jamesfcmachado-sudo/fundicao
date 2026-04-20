24f33af1975b26dde0bd3db1cac5ac3d6dfcabb2
James <jamesfcmachado@gmail.com>
2026-04-19 21:27:10 -0300

    Sempre copia dados OF ao salvar OE

--- app.py
+++ app.py
@@ -3903,8 +3903,8 @@ def pagina_consulta_oes():
                                 "pt":      _edit_qtd * float(_item_d.get("preco_unit", 0) or 0),
                             }
 
-                            # Se OF mudou, busca e copia TODOS os dados da nova OF
-                            if _of_edit != _of_atual_item and _of_edit in _ofs_lista:
+                            # Sempre busca e copia dados da OF (corrige dados incorretos tambem)
+                            if _of_edit in _ofs_lista:
                                 _of_novo = _conn_upd.execute(_text_upd("""
                                     SELECT nome_cliente, numero_pedido, liga,
                                            descricao_peca, numero_modelo,
@@ -3932,23 +3932,23 @@ def pagina_consulta_oes():
                                             preco_unit   = :pu
                                         WHERE id = :id
                                     """), {
-                                        "id":       _item_d["id"],
-                                        "num_of":   _of_edit,
-                                        "qtd":      _edit_qtd,
-                                        "serie":    _edit_serie,
-                                        "corrida":  _edit_corr,
-                                        "cert":     _edit_cert,
-                                        "pt":       _novo_pt,
-                                        "cli":      _of_novo[0] or "",
-                                        "ped":      _of_novo[1] or "",
-                                        "liga":     _of_novo[2] or "",
+                                        "id":        _item_d["id"],
+                                        "num_of":    _of_edit,
+                                        "qtd":       _edit_qtd,
+                                        "serie":     _edit_serie,
+                                        "corrida":   _edit_corr,
+                                        "cert":      _edit_cert,
+                                        "pt":        _novo_pt,
+                                        "cli":       _of_novo[0] or "",
+                                        "ped":       _of_novo[1] or "",
+                                        "liga":      _of_novo[2] or "",
                                         "descricao": _of_novo[3] or "",
-                                        "cod_peca": _of_novo[4] or "",
-                                        "peso":     float(_of_novo[5] or 0),
-                                        "pu":       _novo_pu,
+                                        "cod_peca":  _of_novo[4] or "",
+                                        "peso":      float(_of_novo[5] or 0),
+                                        "pu":        _novo_pu,
                                     })
-                                    st.info(f"Dados copiados da OF {_of_edit}: {_of_novo[3] or ''}")
                             else:
+                                # OF nao encontrada - atualiza apenas campos editaveis
                                 _conn_upd.execute(_text_upd("""
                                     UPDATE oe_item SET
                                         num_of=:num_of, qtd=:qtd, serie=:serie,
